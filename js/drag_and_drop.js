73d199ac7f137c9c3f2c4018046a7b51101504b3
kanagnostopoulos <kanagnostopoulos@atypon.com>
2024-10-25 16:40:34 +0300

    Add support for mobile phones

--- js/drag_and_drop.js
+++ js/drag_and_drop.js
@@ -1,5 +1,8 @@
 function dragSensor(event) {
-    
+    // Determine if it's a touch event
+    if (event.type === "touchstart") {
+        event = event.touches[0];  // Access the first touch point
+    }
     event.dataTransfer.setData('sensor-id', event.currentTarget.id);
     event.dataTransfer.setData('sensor-type', event.currentTarget.getAttribute('data-type'));
     event.currentTarget.classList.add('dragging');
@@ -24,6 +27,10 @@ function dragLeave(event) {
 
 function dropToRoom(event) {
     event.preventDefault();
+    // Determine if it's a touch event
+    if (event.type === "touchend") {
+        event = event.touches[0];  // Access the first touch point
+    }
 
     let sensorId = event.dataTransfer.getData('sensor-id');
     let sensorDiv = document.getElementById(sensorId);
@@ -39,6 +46,9 @@ function dropToRoom(event) {
 
 function dropToAvailable(event) {
     event.preventDefault();
+    if (event.type === "touchend") {
+        event = event.changedTouches[0];
+    }
 
     let sensorId = event.dataTransfer.getData('sensor-id');
     let sensorType = event.dataTransfer.getData('sensor-type');
@@ -52,3 +62,17 @@ function dropToAvailable(event) {
         categoryDiv.classList.remove('droppable');
     })
 }
+
+function handleTouchMove(event) {
+    event.preventDefault();
+    const touch = event.touches[0];
+    const draggedElement = document.querySelector(".dragging"); // Track the currently dragging element
+
+    if (draggedElement) {
+        draggedElement.style.left = `${touch.pageX}px`;
+        draggedElement.style.top = `${touch.pageY}px`;
+    }
+}
+
+// Example usage
+document.addEventListener('touchmove', handleTouchMove);
